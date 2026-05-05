ca3b8a1585024d3bc39a3064fe21637f01e5431b
Israel Tiburcio <agenor.it@gmail.com>
2026-05-05 11:58:43 -0600

    Intro

--- Bimbo/RutaPerfectaApp.swift
+++ Bimbo/RutaPerfectaApp.swift
@@ -2,9 +2,26 @@ import SwiftUI
 
 @main
 struct RutaPerfectaApp: App {
+    @State private var showSplash: Bool = true
+
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            ZStack {
+                MainTabView()
+
+                if showSplash {
+                    SplashView()
+                        .zIndex(1)
+                        .transition(.opacity)
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 3.6) {
+                                withAnimation {
+                                    showSplash = false
+                                }
+                            }
+                        }
+                }
+            }
         }
     }
 }
