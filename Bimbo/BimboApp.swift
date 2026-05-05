d08d4ac9322d77a253aceb7432272661bd4f4f4a
Israel Tiburcio <agenor.it@gmail.com>
2026-05-04 20:17:14 -0600

    Initial Commit

---
+++ Bimbo/BimboApp.swift
@@ -0,0 +1,17 @@
+//
+//  BimboApp.swift
+//  Bimbo
+//
+//  Created by Israel Tiburcio Suchil on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BimboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
